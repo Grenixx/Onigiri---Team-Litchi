37a410bc847781ed46ea3fc3f9871a865bc15cf7
Grenixx <enzomyo@gmail.com>
2026-05-23 02:09:48 +0200

    addede .bat

---
+++ pyinstaller.bat
@@ -0,0 +1,14 @@
+@echo off
+cd Onigiri_client
+pyinstaller --onefile --add-data "data;data" --add-data "scripts;scripts" --icon="..\icone.ico" menu.py
+mkdir ..\..\ClientBuild
+copy dist\menu.exe ..\..\ClientBuild\
+xcopy /E /I /Y data ..\..\ClientBuild\data
+xcopy /E /I /Y scripts ..\..\ClientBuild\scripts
+ 
+cd ..\Onigiri_server
+pyinstaller --onefile --icon="..\icone.ico" server.py
+mkdir ..\..\ServerBuild
+copy dist\server.exe ..\..\ServerBuild\
+xcopy /E /I /Y data ..\..\ServerBuild\data
+pause
